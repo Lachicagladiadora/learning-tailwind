cecabf6205b331e68691cc09ccf6fe05bf4cbecb
Lachicagladiadora <200379@unsaac.edu.pe>
2023-09-30 22:32:16 -0500

    fix: readme and notification

--- src/components/Notification.jsx
+++ src/components/Notification.jsx
@@ -2,15 +2,17 @@ import Bubble from "./Icons/Bubble";
 
 export const Notification = () => {
   return (
-    <div className="bg-gray-50 max-w-sm mx-auto flex items-center rounded-lg p-6 shadow-lg ">
-      <div className="w-12 h-12 flex-shrink-2 shadow-sm">
-        <Bubble className="h-12 w-12" />
-      </div>
-      <div className="ml-6 pt-1">
-        <h4 className="text-xl leading-5 text-black">ChitChat</h4>
-        <p className="text-slate-500  text-base leading-6">
-          You have a new message!
-        </p>
+    <div className="py-5">
+      <div className="bg-gray-50 max-w-sm mx-auto flex items-center rounded-lg p-6 shadow-lg ">
+        <div className="w-12 h-12 flex-shrink-2 shadow-sm">
+          <Bubble className="h-12 w-12" />
+        </div>
+        <div className="ml-6 pt-1">
+          <h4 className="text-xl leading-5 text-black">ChitChat</h4>
+          <p className="text-slate-500  text-base leading-6">
+            You have a new message!
+          </p>
+        </div>
       </div>
     </div>
   );
