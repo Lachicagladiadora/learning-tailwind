5e57d5d026c7da14fecfd8e66c9ddf8129570632
Lachicagladiadora <200379@unsaac.edu.pe>
2024-08-14 23:24:43 -0500

    chore: advance contact list component

---
+++ src/components/ContactsList.jsx
@@ -0,0 +1,23 @@
+//make a array with contacts data
+
+
+const Contact = () => {
+  return (
+    <div>
+      <section>
+        <img src="" alt="" />
+      </section>
+      <section>
+        name
+        correo
+      </section>
+    </div>)
+}
+
+export const ContactsList = () => {
+  return (
+    <div>
+
+    </div>
+  )
+}
