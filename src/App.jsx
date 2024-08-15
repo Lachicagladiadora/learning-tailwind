83ed322965bd2f2d8038106336d649ebec0bb987
Lachicagladiadora <200379@unsaac.edu.pe>
2024-08-14 23:13:13 -0500

    fix: background and other little things

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,14 @@ import { Profile } from "./components/Profile";
 function App() {
   return (
     <section className="h-full w-full flex gap-5 justify-center items-center flex-col">
-      <h1 className="text-3xl font-bold underline text-center">
-        Learning Tailwind
+      <h1 className="text-white text-3xl font-bold text-center">
+        Learning tailwind
       </h1>
-      <div className="w-4/5 p-8 bg-slate-900">
+      <div className="w-4/5 p-8">
+        <h2 className="text-cyan-400 text-xl">Utility-First Fundamentals</h2>
         <Notification />
         <Profile />
+        <h2 className="text-cyan-400 text-xl">Handling Hover, Focus, and Other States</h2>
         <ButtonPrimary />
         <ButtonActive />
       </div>
