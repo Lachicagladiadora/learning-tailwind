750a73e4fc25a442db9428084e60c36a5094a2fb
Lachicagladiadora <dev.roseny@gmail.com>
2024-10-04 10:52:41 -0500

    chore: peer class

--- src/components/PublishedStatus.jsx
+++ src/components/PublishedStatus.jsx
@@ -1,17 +1,11 @@
-import { useState } from "react"
-
 export const PublishedStatus = () => {
-  const [draft, setDraft] = useState(false)
   return (
-    <div className='bg-white p-4 '>
-      <header>Published status</header>
-      <section>
-        <div><label htmlFor="draft">Draft</label> <input id='draft' type="checkbox" value={draft} onChange={e => setDraft(e.target.value)} /></div>
-        <div><label htmlFor="published">Published</label> <input id='published' type="checkbox" value={!draft} /></div>
-      </section>
-      {draft && <p>Drafts are only visible to administrators.</p>}
-      {!draft && <p>Your post will be publicly visible on your site.
-      </p>}
-    </div>
+    <div className='w-[448px] bg-white p-8 text-md text-gray-400'>
+      <legend className="w-full border-b border-b-gray-400 text-gray-400 pb-2 font-semibold">Published status</legend>
+      <input id='draft' type="radio" className="my-4 mr-2 peer/draft" name="status" checked /><label htmlFor="draft" className="text-gray-400 my-4 mr-8 peer-checked:/draft:text-sky-500">Draft</label>
+      <input id='published' type="radio" className="my-4 mr-2 peer/published" name="status" /><label htmlFor="published" className="text-gray-400 my-4 mr-8 peer-checked:/published:text-sky-500">Published</label>
+      <p className="text-sm text-gray-500 hidden peer-checked/draft:block">Drafts are only visible to administrators.</p>
+      <p className="text-sm text-gray-500 hidden peer-checked/published:block">Your post will be publicly visible on your site.</p>
+    </div >
   )
 }
