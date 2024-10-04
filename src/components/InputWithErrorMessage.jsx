5dd449e648ffede8f60947d7894c365fe9295d65
Lachicagladiadora <dev.roseny@gmail.com>
2024-10-04 10:07:17 -0500

    fix: id from email

--- src/components/InputWithErrorMessage.jsx
+++ src/components/InputWithErrorMessage.jsx
@@ -5,8 +5,8 @@ export const InputWithErrorMessage = () => {
   const [emailValue, setEmailValue] = useState('')
   return (
     <div className={`w-[384px] h-[134px] flex flex-col justify-center gap-1 bg-white px-8 py-4 text-sm ${emailValue === EMAIL_REGEX_VALIDATE || !emailValue ? "text-black" : "text-red-600"}`}>
-      <label htmlFor="email">Email</label>
-      <input id="email" type="email" value={emailValue} onChange={(e) => setEmailValue(e.target.value)} placeholder="yourEmail@email.com"
+      <label htmlFor="email-with-error">Email</label>
+      <input id="email-with-error" type="email" value={emailValue} onChange={(e) => setEmailValue(e.target.value)} placeholder="yourEmail@email.com"
         className={`peer border-[2px] outline-none px-3 py-1.5 rounded-md ${emailValue === EMAIL_REGEX_VALIDATE || !emailValue ? 'border-sky-500 text-black' : "border-red-600 text-red-600"}`} />
       <p className="invisible peer-invalid:visible">Please provide a valid email address</p>
     </div>
