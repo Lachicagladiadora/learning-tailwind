6509090a645a020b6f191bb6bb331b70590258fd
Lachicagladiadora <dev.roseny@gmail.com>
2024-10-07 22:01:22 -0500

    chore: add styles for update categories

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,12 +1,12 @@
 // eslint-disable-next-line react/prop-types
 export const Categories = ({ categories }) => {
   return (
-    <div className="w-[448px] bg-[#1e293b] text-slate-50 p-5">
+    <div className="w-[448px] bg-[#1e293b] text-slate-50 p-5 text-sm">
       <h3>Categories</h3>
       <ul className="w-full *:rounded-full *:px-2 *:py-0.5 *:border-[2px] *-border-sky-100 *:bg-sky-50 dark:text-sky-300 dark:*:border-sky-500/15 dark:*:bg-sky-500/10 *:inline flex flex-wrap gap-2 items-center">
         {/* eslint-disable-next-line react/prop-types */}
         {categories.map((c, i) => (
-          <li key={i} className="w-auto rounded-full px-2 py-1 h-8 border border-sky-500 inline bg-sky-500/10">{c}</li>
+          <li key={i} className="w-auto rounded-full px-2 py-0.5 h-8 border border-sky-500 bg-sky-500/10 flex items-center justify-center">{c}</li>
         ))}
       </ul>
     </div>
