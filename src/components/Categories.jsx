4eb4231f7546504cc282a4f78f6f068f1d5276c3
Lachicagladiadora <dev.roseny@gmail.com>
2024-12-04 23:54:15 -0500

    fix: categories

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -6,7 +6,7 @@ export const Categories = ({ categories }) => {
       <ul className="w-full *:rounded-full *:px-2 *:py-0.5 *:border-[2px] *-border-sky-100 *:bg-sky-50 dark:text-sky-300 dark:*:border-sky-500/15 dark:*:bg-sky-500/10 *:inline flex flex-wrap gap-2 items-center">
         {/* eslint-disable-next-line react/prop-types */}
         {categories.map((c, i) => (
-          <li key={i} className="w-auto rounded-full px-2 py-0.5 h-8 border border-sky-500 bg-sky-500/10 flex items-center justify-center">{c}</li>
+          <li key={i} className="w-auto rounded-full px-3 py-0.5 text-center text-xs font-medium border border-sky-500 bg-sky-500/20 flex items-center justify-center">{c}</li>
         ))}
       </ul>
     </div>
